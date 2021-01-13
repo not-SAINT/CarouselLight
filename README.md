61e21c3e4a8c81da4e2cf2da973b14a1ec11f857
Aleksandr Sharikov <notsaint.mail@gmail.com>
2021-01-13 10:45:49 +0300

    init: start project - CarouselLight

---
+++ README.md
@@ -0,0 +1,1 @@
+# CarouselLight - lightweight React component
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>CarouselLight demo</h1>
+    </div>
+  );
+};
+
+export default App;
